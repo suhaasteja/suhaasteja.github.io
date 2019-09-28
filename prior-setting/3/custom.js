8a5373eee1f5c9208653b9918c3340dd29132ccf
abhsarma <abhsarma@umich.edu>
2019-09-28 14:57:14 -0500

    update 9/28

--- prior-setting/3/custom.js
+++ prior-setting/3/custom.js
@@ -1,13 +1,25 @@
 var container, alpha, beta;
 
-function get_priors() {
+function next_page(url){
+  var timeSpentOnPage = TimeMe.getTimeOnCurrentPageInSeconds();
+  url = url+'?t1='+timeSpentOnPage;
+  console.log(timeSpentOnPage);
+  
+  window.open(url, "_self");
+}
+
+function get_priors(condition) {
   container = document.querySelectorAll('.r2d3');
   str_alpha = container[0].shadowRoot.querySelector(".text-eff-prior").innerHTML;
   prior_alpha = str_alpha.split(" ")[2];
   str_beta = container[1].shadowRoot.querySelector(".text-eff-prior").innerHTML;
   prior_beta = str_beta.split(" ")[2];
   
-  url = 'https://umich.qualtrics.com/jfe/form/SV_ebP6FaVE8j2JzZH?alpha='+prior_alpha+'&beta='+prior_beta;
+  var urlParams = new URLSearchParams(window.location.search);
+  var t1 = urlParams.get('t1');
+  var t2 = TimeMe.getTimeOnCurrentPageInSeconds();
+  
+  url = 'https://umich.qualtrics.com/jfe/form/SV_ebP6FaVE8j2JzZH?cond='+condition+'&alpha='+prior_alpha+'&beta='+prior_beta+'&t1='+t1+'&t2='+t2 ;
   
   window.open(url, "_blank");
 }
@@ -19,6 +31,8 @@ function show_section(id) {
     $('div#part-2').css('visibility', 'visible');
   }
   
+  console.log(id);
+  
   $('button.'+id+'-btn').hide();
 }
 
@@ -26,12 +40,12 @@ $("#btn-pg-2").click(function(){
 		text = $(this).text().split(" ")[0];
     
     if (text == "Show") {
-    		$('div#experiment-details').css('display', 'inherit');
+    		$('div#description').css('display', 'inherit');
     } else {
-    		$('div#experiment-details').css('display', 'none');
+    		$('div#description').css('display', 'none');
     }
     
 		$(this).text(function(i, text){
-          return text === "Show experiment details" ? "Hide experiment details" : "Show experiment details";
+          return text === "Show description" ? "Hide description" : "Show description";
      });
 } );
