3fa5c9f2694ee9a6ca751891b9a4e5d55c6f70c7
abhsarma <abhsarma@umich.edu>
2020-05-04 13:53:47 -0500

    removed stuff

--- algo/algoplay/js/scenario.js
+++
@@ -1,9 +0,0 @@
-$('div.task-placeholder').hover(
-        function() {
-            var imgsrc = $(this).children().attr('src').slice(0, -4);
-            $(this).children().attr('src', imgsrc+ '-hover.png');
-    }, function() {
-            var imgsrc = $(this).children().attr('src').slice(0, -10);
-            $(this).children().attr('src', imgsrc+ '.png');
-    }
-);
